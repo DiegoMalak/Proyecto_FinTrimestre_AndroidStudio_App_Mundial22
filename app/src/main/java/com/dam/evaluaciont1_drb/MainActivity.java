1a9dee1c4c584fcf950213227d73d59bea1f5550
DiegoMalakDev <diego.rodriguezbellido@gmail.com>
2022-12-04 13:22:26 +0100

    Añadimos comentarios en la clase MainActivity.

--- app/src/main/java/com/dam/evaluaciont1_drb/MainActivity.java
+++ app/src/main/java/com/dam/evaluaciont1_drb/MainActivity.java
@@ -13,25 +13,33 @@ public class MainActivity extends AppCompatActivity {
     private Button btnRegistro;
     private Button btnConsulta;
 
+    // No declaro el TextView porque no lo voy a usar.
+
+    // Hacemos el OnCreate de la actividad principal.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicializamos los elementos de la interfaz principal.
         btnRegistro = findViewById(R.id.btnRegistrar);
         btnConsulta = findViewById(R.id.btnConsultar);
 
+        // Creamos el Listener del boton de registro.
         btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Creamos el Intent para ir a la actividad de registro.
                 Intent i = new Intent(getApplicationContext(), RegistroActivity.class);
                 startActivity(i);
             }
         });
 
+        // Creamos el Listener del boton de consulta.
         btnConsulta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Creamos el Intent para ir a la actividad de consulta.
                 Intent i = new Intent(getApplicationContext(), ConsultaActivity.class);
                 startActivity(i);
             }
